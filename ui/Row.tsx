f835040580674edb5d0b6bff2e4dbe72162bcb0b
max randle <maxrandle95@gmail.com>
2026-02-22 22:17:21 +1300

    add shorter version for colo rewards

--- ui/Row.tsx
+++ ui/Row.tsx
@@ -1,9 +1,21 @@
-export const Row: React.FC<React.ComponentPropsWithoutRef<"div">> = ({
+import { cx } from "tailwind-variants";
+
+type Props = {
+  justify?: "center" | "evenly";
+};
+
+const justifyVariants: Record<NonNullable<Props["justify"]>, string> = {
+  center: "justify-center",
+  evenly: "justify-evenly",
+};
+
+export const Row: React.FC<React.ComponentPropsWithoutRef<"div"> & Props> = ({
+  justify = "evenly",
   ...props
 }) => {
   return (
     <div
-      className="flex flex-row gap-4 items-center justify-evenly"
+      className={cx("flex flex-row gap-4 items-center", justifyVariants[justify])}
       {...props}
     />
   );
